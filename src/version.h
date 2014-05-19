996d0f9c83983d01ce9ddb35b3010c333a90a2ff
Sebastian Freundt <freundt@ga-group.nl>
2014-05-19 12:17:39 +0000

    initial import of toolchain
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ src/version.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDED_version_h_
+#define INCLUDED_version_h_
+
+extern const char echse_version_string[];
+#define package_version	echse_version_string
+
+#endif	/* INCLUDED_version_h_ */
