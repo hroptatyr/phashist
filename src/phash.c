21ef82a1e5427a558b2a2797de9afec0cb9aebe7
Sebastian Freundt <freundt@ga-group.nl>
2014-09-11 06:58:47 +0000

    minor, import Bob's lookup() hash (as bob()) for fun and profit

--- src/phash.c
+++ src/phash.c
@@ -92,12 +92,114 @@ jsw(phkey_t data, size_t dlen, phash_t prev)
 	return v;
 }
 
+static phash_t
+bob(phkey_t data, size_t dlen, phash_t prev)
+{
+/*
+--------------------------------------------------------------------
+mix -- mix 3 32-bit values reversibly.
+For every delta with one or two bit set, and the deltas of all three
+  high bits or all three low bits, whether the original value of a,b,c
+  is almost all zero or is uniformly distributed,
+* If mix() is run forward or backward, at least 32 bits in a,b,c
+  have at least 1/4 probability of changing.
+* If mix() is run forward, every bit of c will change between 1/3 and
+  2/3 of the time.  (Well, 22/100 and 78/100 for some 2-bit deltas.)
+mix() was built out of 36 single-cycle latency instructions in a 
+  structure that could supported 2x parallelism, like so:
+      a -= b; 
+      a -= c; x = (c>>13);
+      b -= c; a ^= x;
+      b -= a; x = (a<<8);
+      c -= a; b ^= x;
+      c -= b; x = (b>>13);
+      ...
+  Unfortunately, superscalar Pentiums and Sparcs can't take advantage 
+  of that parallelism.  They've also turned some of those single-cycle
+  latency instructions into multi-cycle latency instructions.  Still,
+  this is the fastest good hash I could find.  There were about 2^^68
+  to choose from.  I only looked at a billion or so.
+--------------------------------------------------------------------
+*/
+#define mix(a, b, c)					\
+	do {						\
+		a -= b, a -= c, a ^= (c >> 13U);	\
+		b -= c, b -= a, b ^= (a << 8U);		\
+		c -= a, c -= b, c ^= (b >> 13U);	\
+		a -= b, a -= c, a ^= (c >> 12U);	\
+		b -= c, b -= a, b ^= (a << 16U);	\
+		c -= a, c -= b, c ^= (b >> 5U);		\
+		a -= b, a -= c, a ^= (c >> 3U);		\
+		b -= c, b -= a, b ^= (a << 10U);	\
+		c -= a, c -= b, c ^= (b >> 15U);	\
+	} while (0)
+
+	register phash_t a = 0x9e3779b9;
+	register phash_t b = 0x9e3779b9;
+	register phash_t c = prev;
+
+	/* handle most of the key */
+	for (; dlen >= 12U; data += 12U, dlen -= 12U) {
+		a += data[0U] +
+			((phash_t)data[1U] << 8U) +
+			((phash_t)data[2U] << 16U) +
+			((phash_t)data[3U] << 24U);
+		b += data[4U] +
+			((phash_t)data[5U] << 8U) +
+			((phash_t)data[6U] << 16U) +
+			((phash_t)data[7U] << 24);
+		c += data[8U] +
+			((phash_t)data[9U] << 8U) +
+			((phash_t)data[10U] << 16U) +
+			((phash_t)data[11U] << 24U);
+		mix(a, b, c);
+	}
+
+	/* handle the last 11 bytes */
+	c += dlen;
+	switch (dlen) {
+	case 11U:
+		c += ((phash_t)data[10U] << 24U);
+	case 10U:
+		c += ((phash_t)data[9U] << 16U);
+	case 9U:
+		c += ((phash_t)data[8U] << 8U);
+
+		/* the first byte of c is reserved for the length */
+	case 8U:
+		b += ((phash_t)data[7U] << 24U);
+	case 7U:
+		b += ((phash_t)data[6U] << 16U);
+	case 6U:
+		b += ((phash_t)data[5U] << 8U);
+	case 5U:
+		b += data[4U];
+
+	case 4U:
+		a += ((phash_t)data[3U] << 24U);
+	case 3U:
+		a += ((phash_t)data[2U] << 16U);
+	case 2U:
+		a += ((phash_t)data[1U] << 8U);
+	case 1U:
+		a += data[0U];
+
+		/* case 0: nothing left to add */
+	case 0U:
+	default:
+		break;
+	}
+	mix(a, b, c);
+	/* report the result */
+	return c;
+}
+
 
 /* public API */
 phash_t
 phash(phkey_t key, size_t len, phash_t salt)
 {
-	return bingo(key, len, salt);
+	return bob(key, len, salt);
 }
 
 /* phash.c ends here */
